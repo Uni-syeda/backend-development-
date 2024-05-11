84c41112f41a12da7e0ef9d9da049427e7da17c5
syeda shakera <unisyeds808@gmail.com>
2024-05-11 12:20:38 -0600

    code added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const session = require("express-session");
 const app = express();
 
 //create a port number
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 
 //Require morgan as Middleware
 const morgan = require("morgan");
